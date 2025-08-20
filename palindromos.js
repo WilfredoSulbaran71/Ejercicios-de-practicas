1066cc74200ccc0eb127bdf048cf86130193c9ab
Wilbaran <sulbaran.wilfredo71@gmail.com>
2025-08-20 19:33:01 -0400

    Actualizacion del ejercicio 5, se agrego COD para validar datos

--- palindromos.js
+++ palindromos.js
@@ -1,6 +1,13 @@
 /*Determinar si una frace o palabra es palindromo*/
 
 function textoPalindromo(cadena){
+    //validacion de los datos
+    if(typeof cadena !=='string'){
+        return "Error: la entrada no es una cadena de texto"
+    }
+    if(cadena.length===0){
+        return "Error: la cadena esta vacia"
+    }
     //llevar todo a minusculas y definr parametro de busqueda
     const cadenaLimpia = cadena.toLowerCase().replace(/[^a-z0-9]/g, '');
     //invertir la cadena
